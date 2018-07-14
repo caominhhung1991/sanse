8af0ca4588041603f180eb24723d9e0e4596ae22
nghiatranJ <30631186+nghiatranJ@users.noreply.github.com>
2018-07-14 19:51:16 +0700

    Delete sanse.action.js

--- sanse/src/containers/sanse/store/sanse.action.js
+++
@@ -1,8 +0,0 @@
-import * as SanSeActionTypes from './sanse.type';
-import instanceAxios from '../../../src/axios-orders';
-
-export const hello = () => {
-  return {
-    type: SanSeActionTypes.HELLO
-  };
-};
